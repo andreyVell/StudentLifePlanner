52c0473101c79d2f2578631128c1196e9b93b032
andreyVell <andreyperm109@mail.ru>
2023-02-16 13:20:32 +0500

    Добавьте файлы проекта.

---
+++ Slp.Data/Entities/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Slp.Data.Entities
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public Guid CreatedBy { get; set; }
+        public DateTime? ModifyAt { get; set; }
+        public Guid? ModifyBy { get; set; }
+    }
+}
